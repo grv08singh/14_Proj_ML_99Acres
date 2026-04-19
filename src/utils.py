bf04e2fca80f50a9dbacac28a4cab5d480f86ee6
Gaurav Singh <grv08singh@gmail.com>
2026-04-19 16:32:07 +0530

    data Transformation done

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,20 @@
- 
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+
+import numpy as np
+import pandas as pd
+import dill
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+
+    except Exception as e:
+        logging.error("Error occurred while saving object: %s", str(e))
+        raise CustomException(e, sys)
