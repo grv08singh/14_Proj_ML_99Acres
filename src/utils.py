fef6d896ba7174b77dd3a2b546bd9a9b92571031
Gaurav Singh <grv08singh@gmail.com>
2026-04-19 18:51:26 +0530

    model training

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from src.logger import logging
 
 import numpy as np
 import pandas as pd
+from sklearn.metrics import r2_score
 import dill
 
 def save_object(file_path, obj):
@@ -17,4 +18,19 @@ def save_object(file_path, obj):
 
     except Exception as e:
         logging.error("Error occurred while saving object: %s", str(e))
+        raise CustomException(e, sys)
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[model_name] = test_model_score
+
+        return report
+
+    except Exception as e:
+        logging.error("Error occurred during model evaluation: %s", str(e))
         raise CustomException(e, sys)
