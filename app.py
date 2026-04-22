c9a6ae5a588634940acc55ae1f02832549a3ac85
grv08singh <grv08singh@gmail.com>
2026-04-22 17:34:45 +0530

    Auto-sync: 2026-04-22 17:34:40.11

--- app.py
+++ app.py
@@ -4,81 +4,62 @@ from src.pipeline.predict_pipeline import PredictPipeline
 
 def prediction_query():
     # Create input fields for user to enter features
-    area_sqm = st.number_input("Area (in sqm)", min_value=0.0)
-    bhk = st.number_input("BHK", min_value=0, step=1)
-    bathroom = st.number_input("Bathroom", min_value=0, step=1)
-    balcony = st.number_input("Balcony", min_value=0, step=1)
-    addl_room = st.number_input("Additional Room", min_value=0, step=1)
-    sector = st.number_input("Sector", min_value=0, step=1)
-    landmark_count = st.number_input("Landmark Count", min_value=0, step=1)
-    has_hospital_or_clinic = st.checkbox("Has Hospital or Clinic")
-    has_police_stn = st.checkbox("Has Police Station")
-    has_fire_stn = st.checkbox("Has Fire Station")
-    has_mall = st.checkbox("Has Mall")
-    has_market = st.checkbox("Has Market")
-    has_park = st.checkbox("Has Park")
-    has_bank_or_atm = st.checkbox("Has Bank or ATM")
-    has_institute = st.checkbox("Has Institute")
-    has_airport = st.checkbox("Has Airport")
-    has_rly_stn = st.checkbox("Has Railway Station")
-    has_metro_stn = st.checkbox("Has Metro Station")
-    has_expy = st.checkbox("Has Expressway")
-    has_petrol_pump = st.checkbox("Has Petrol Pump")
-    has_hotel = st.checkbox("Has Hotel")
-    has_temple_or_church = st.checkbox("Has Temple or Church")
-    has_famous_chowk = st.checkbox("Has Famous Chowk")
-    has_club = st.checkbox("Has Club")
-    has_stadium = st.checkbox("Has Stadium")
-    has_restro_cafe = st.checkbox("Has Restaurant or Cafe")
-    area_type = st.selectbox("Area Type", ["Type 1", "Type 2", "Type 3"])
-    floor_rise = st.selectbox("Floor Rise", ["Low", "Medium", "High"])
-    age = st.selectbox("Age of Property", ["New", "Old"])
-    society = st.text_input("Society Name")
-    city = st.text_input("City Name")
+    st.subheader("Choose your preferences for the property:")
+    all_sectors = [1, 2, 3, 4, 5, 7, 9, 10, 12, 14, 15, 21, 22, 23, 24, 28, 30
+                   , 31, 33, 36, 37, 38, 39, 40, 41, 43, 45, 47, 48, 49, 50, 51
+                   , 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 65, 66, 67
+                   , 68, 69, 70, 71, 72, 73, 74, 76, 77, 78, 79, 80, 81, 82, 83
+                   , 84, 85, 86, 88, 89, 90, 91, 92, 93, 95, 99, 102, 103, 104
+                   , 105, 106, 107, 108, 109, 110, 111, 112, 113]
+
+
+    col1,col2,col3,col4 = st.columns(4)
+    with col1:
+        bhk = st.selectbox("Bedroom", options=list(range(1, 11)))
+        sector = st.selectbox("Sector", options=all_sectors)
+    with col2:
+        floor_rise = st.selectbox("Floor Rise", ["Low", "Mid", "High"])
+        age = st.selectbox("Property Age", ["New", "Mid-Aged", "Old", "Under Construction"])
+
+
+    st.subheader("Nearby Landmarks:")
+    col5,col6,col7,col8,col9 = st.columns(5)
+    with col5:
+        has_market = st.checkbox("Market")
+        has_mall = st.checkbox("Mall")
+        has_park = st.checkbox("Park")
+        has_institute = st.checkbox("School or College")
+    with col6:
+        has_metro_stn = st.checkbox("Metro Station")
+        has_airport = st.checkbox("Airport")
+        has_bank_or_atm = st.checkbox("Bank or ATM")
+        has_hospital_or_clinic = st.checkbox("Hospital")
 
     query_dict = {
-        "area_sqm": area_sqm,
         "bhk": bhk,
-        "bathroom": bathroom,
-        "balcony": balcony,
-        "addl_room": addl_room,
         "sector": sector,
-        "landmark_count": landmark_count,
+        "floor_rise": floor_rise,
+        "age": age,
         "has_hospital_or_clinic": has_hospital_or_clinic,
-        "has_police_stn": has_police_stn,
-        "has_fire_stn": has_fire_stn,
         "has_mall": has_mall,
         "has_market": has_market,
         "has_park": has_park,
         "has_bank_or_atm": has_bank_or_atm,
         "has_institute": has_institute,
         "has_airport": has_airport,
-        "has_rly_stn": has_rly_stn,
-        "has_metro_stn": has_metro_stn,
-        "has_expy": has_expy,
-        "has_petrol_pump": has_petrol_pump,
-        "has_hotel": has_hotel,
-        "has_temple_or_church": has_temple_or_church,
-        "has_famous_chowk": has_famous_chowk,
-        "has_club": has_club,
-        "has_stadium": has_stadium,
-        "has_restro_cafe": has_restro_cafe,
-        "area_type": area_type,
-        "floor_rise": floor_rise,
-        "age": age,
-        "society": society,
-        "city": city
+        "has_metro_stn": has_metro_stn
     }
     return query_dict
 
 def price_prediction():
-    st.title('Gurgaon Real Estate Price Prediction')
+    st.header('Gurgaon Real Estate Price Prediction')
     pred_query_dict = prediction_query()
     pred_query_df = pd.DataFrame([pred_query_dict])
 
-    pp=PredictPipeline()
-    pred=pp.predict(pred_query_df)
-    st.write(f"Predicted Price: INR {pred[0]} crore")
+    if st.button("Predict Price"):
+        pp=PredictPipeline()
+        pred=pp.predict(pred_query_df)
+        st.write(f"Predicted Price: INR {pred[0]} crore")
 
     st.write("Recommendations based on your input:")
     recommendation_system(pred_query_df)
@@ -93,13 +74,11 @@ def overall_analysis():
 
 def main():
     st.set_page_config(layout='wide',page_title='Real Estate Project',page_icon='🏠')
-    st.title('Gurgaon Real Estate Project')
-    st.sidebar.title('Gurgaon Real Estate Project')
-    option = st.sidebar.selectbox('Select One',['Price Prediction','Recommendation System','Analytics'])
-    st.title('Gurgaon Real Estate Project')
+    st.title('Real Estate - Machine Learning Project')
+    st.sidebar.title('Gurgaon City')
+    option = st.sidebar.radio("Select One:", ['Price Prediction', 'Recommendation System', 'Analytics'])
 
     if option == 'Price Prediction':
-        st.title('Gurgaon Real Estate Price Prediction')
         price_prediction()
     elif option == 'Recommendation System':
         st.title('Gurgaon Real Estate Recommendation System')
@@ -126,52 +105,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-        data = CustomData(
-            area_sqm = float(request.form.get('area_sqm')),
-            bhk = int(request.form.get('bhk')),
-            bathroom = int(request.form.get('bathroom')),
-            balcony = int(request.form.get('balcony')),
-            addl_room = int(request.form.get('addl_room')),
-            sector = int(request.form.get('sector')),
-            landmark_count = int(request.form.get('landmark_count')),
-            has_hospital_or_clinic = bool(request.form.get('has_hospital_or_clinic')),
-            has_police_stn = bool(request.form.get('has_police_stn')),
-            has_fire_stn = bool(request.form.get('has_fire_stn')),
-            has_mall = bool(request.form.get('has_mall')),
-            has_market = bool(request.form.get('has_market')),
-            has_park = bool(request.form.get('has_park')),
-            has_bank_or_atm = bool(request.form.get('has_bank_or_atm')),
-            has_institute = bool(request.form.get('has_institute')),
-            has_airport = bool(request.form.get('has_airport')),
-            has_rly_stn = bool(request.form.get('has_rly_stn')),
-            has_metro_stn = bool(request.form.get('has_metro_stn')),
-            has_expy = bool(request.form.get('has_expy')),
-            has_petrol_pump = bool(request.form.get('has_petrol_pump')),
-            has_hotel = bool(request.form.get('has_hotel')),
-            has_temple_or_church = bool(request.form.get('has_temple_or_church')),
-            has_famous_chowk = bool(request.form.get('has_famous_chowk')),
-            has_club = bool(request.form.get('has_club')),
-            has_stadium = bool(request.form.get('has_stadium')),
-            has_restro_cafe = bool(request.form.get('has_restro_cafe')),
-            area_type = request.form.get('area_type'),
-            floor_rise = request.form.get('floor_rise'),
-            age = request.form.get('age'),
-            society = request.form.get('society'),
-            city = request.form.get('city')
-        )
-
-        pred_df = data.get_data_as_dataframe()
-        print(pred_df)
-
-        predict_pipeline=PredictPipeline()
-        results=predict_pipeline.predict(pred_df)
-        return render_template("home.html", results=results[0])
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -27,105 +27,48 @@ def predict(self, features):
 
 class CustomData:
     def __init__(self,
-                    area_sqm: float,
                     bhk: int,
-                    bathroom: int,
-                    balcony: int,
-                    addl_room: int,
                     sector: int,
-                    landmark_count: int,
+                    floor_rise: str,
+                    age: str,
                     has_hospital_or_clinic: bool,
-                    has_police_stn: bool,
-                    has_fire_stn: bool,
                     has_mall: bool,
                     has_market: bool,
                     has_park: bool,
                     has_bank_or_atm: bool,
                     has_institute: bool,
                     has_airport: bool,
-                    has_rly_stn: bool,
-                    has_metro_stn: bool,
-                    has_expy: bool,
-                    has_petrol_pump: bool,
-                    has_hotel: bool,
-                    has_temple_or_church: bool,
-                    has_famous_chowk: bool,
-                    has_club: bool,
-                    has_stadium: bool,
-                    has_restro_cafe: bool,
-                    area_type: bool,
-                    floor_rise: str,
-                    age: str,
-                    society: str,
-                    city: str):
+                    has_metro_stn: bool):
 
-        self.area_sqm = area_sqm
         self.bhk = bhk
-        self.bathroom = bathroom
-        self.balcony = balcony
-        self.addl_room = addl_room
         self.sector = sector
-        self.landmark_count = landmark_count
+        self.floor_rise = floor_rise
+        self.age = age
         self.has_hospital_or_clinic = has_hospital_or_clinic
-        self.has_police_stn = has_police_stn
-        self.has_fire_stn = has_fire_stn
         self.has_mall = has_mall
         self.has_market = has_market
         self.has_park = has_park
         self.has_bank_or_atm = has_bank_or_atm
         self.has_institute = has_institute
         self.has_airport = has_airport
-        self.has_rly_stn = has_rly_stn
         self.has_metro_stn = has_metro_stn
-        self.has_expy = has_expy
-        self.has_petrol_pump = has_petrol_pump
-        self.has_hotel = has_hotel
-        self.has_temple_or_church = has_temple_or_church
-        self.has_famous_chowk = has_famous_chowk
-        self.has_club = has_club
-        self.has_stadium = has_stadium
-        self.has_restro_cafe = has_restro_cafe
-        self.area_type = area_type
-        self.floor_rise = floor_rise
-        self.age = age
-        self.society = society
-        self.city = city
 
 
     def get_data_as_dataframe(self):
         try:
             custom_data_input_dict = {
-                "area_sqm": [self.area_sqm],
                 "bhk": [self.bhk],
-                "bathroom": [self.bathroom],
-                "balcony": [self.balcony],
-                "addl_room": [self.addl_room],
                 "sector": [self.sector],
-                "landmark_count": [self.landmark_count],
+                "floor_rise": [self.floor_rise],
+                "age": [self.age],
                 "has_hospital_or_clinic": [self.has_hospital_or_clinic],
-                "has_police_stn": [self.has_police_stn],
-                "has_fire_stn": [self.has_fire_stn],
                 "has_mall": [self.has_mall],
                 "has_market": [self.has_market],
                 "has_park": [self.has_park],
                 "has_bank_or_atm": [self.has_bank_or_atm],
                 "has_institute": [self.has_institute],
                 "has_airport": [self.has_airport],
-                "has_rly_stn": [self.has_rly_stn],
-                "has_metro_stn": [self.has_metro_stn],
-                "has_expy": [self.has_expy],
-                "has_petrol_pump": [self.has_petrol_pump],
-                "has_hotel": [self.has_hotel],
-                "has_temple_or_church": [self.has_temple_or_church],
-                "has_famous_chowk": [self.has_famous_chowk],
-                "has_club": [self.has_club],
-                "has_stadium": [self.has_stadium],
-                "has_restro_cafe": [self.has_restro_cafe],
-                "area_type": [self.area_type],
-                "floor_rise": [self.floor_rise],
-                "age": [self.age],
-                "society": [self.society],
-                "city": [self.city]
+                "has_metro_stn": [self.has_metro_stn]
             }
 
             return pd.DataFrame(custom_data_input_dict)
